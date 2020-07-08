dbaf44d2665a17f95df7f19370e4fd521e7608c3
krishnarbk <66946253+krishnarbk@users.noreply.github.com>
2020-07-08 12:39:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Author author=new Author("Sachin","sachin@gmail.com",'F');
+		Book book=new Book("Dell","virat","duke@gmail.com",'F',24.33,8);
+		System.out.println(book.toString());
+
+	}
+}
